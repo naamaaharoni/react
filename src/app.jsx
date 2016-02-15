efb13d9fe81f87ef48f44f09e2ead82bfc56f4c3
Naama Aharoni <naama.aharoni2@gmail.com>
2016-02-15 10:54:12 +0200

    react ex5

--- src/app.jsx
+++ src/app.jsx
@@ -18,6 +18,7 @@ requirejs(['lodash', 'react', 'reactDom'], function (_, React, ReactDOM) {
         React.createElement('li', null, 'world'));
     ReactDOM.render(elem, root);
 
+
     //Ex2
     root = document.getElementById('ex2');
     elem = <ul>
@@ -26,6 +27,7 @@ requirejs(['lodash', 'react', 'reactDom'], function (_, React, ReactDOM) {
         <li>example</li>
     </ul>;
     ReactDOM.render(elem, root);
+
 
     //Ex3
     var data = ['Hello', 'there', 'world!'];
@@ -37,24 +39,69 @@ requirejs(['lodash', 'react', 'reactDom'], function (_, React, ReactDOM) {
     }</ul>;
     ReactDOM.render(elem, root);
 
+
     //ex4
     root = document.getElementById('ex4');
     var List = React.createClass({
+        displayName: 'List',
         render: function () {
             return <ul>{
-                _.map(this.props.items,function(item, index){
+                _.map(this.props.items, function (item, index) {
                     return <ListItem item={item} key={index}></ListItem>
                 })
             }</ul>;
         }
     });
     var ListItem = React.createClass({
+        displayName: 'ListItem',
         render: function () {
             return <li>{this.props.item}</li>
         }
     });
-    ReactDOM.render(React.createElement(List, {
-        items: data
-    }), root);
+    elem = <List items={data}/>;
+    ReactDOM.render(elem, root);
+
+
+    //ex5
+    data = ['A', 'C', 'B'];
+    root = document.getElementById('ex5');
+    var SortBtn = React.createClass({
+        render: function () {
+            return <button onClick={this.props.sort}><i className={'fa fa-sort-' + this.props.sortClass}></i></button>;
+        }
+    });
+    var SortedList = React.createClass({
+        displayName: 'List',
+        getInitialState: function () {
+            return {
+                order: 'desc',
+            };
+        },
+        sort: function () {
+            this.setState({
+                order: this.state.order === 'asc' ? 'desc' : 'asc'
+            });
+        },
+        renderItems: function () {
+            return _.chain(this.props.items)
+                .orderBy(_.identity,this.state.order)
+                .map(function (item, index) {
+                    return <ListItem item={item} key={index}></ListItem>
+                })
+                .value()
+        },
+        render: function () {
+            return (
+                <div>
+                    <SortBtn sortClass={this.state.order} sort={this.sort}/>
+                    <ul>{
+                        this.renderItems()
+                    }</ul>
+                </div>
+            );
+        }
+    });
+    elem = <SortedList items={data}/>;
+    ReactDOM.render(elem, root);
 
 });
